396a20383981f79a1e8b4e18b467aed12f3fced3
cerlinmathew <cerlinmathew2001@gmail.com>
2026-04-01 16:57:31 +0530

    feat: added calendar

--- android/app/src/components/CalendarComponent.tsx
+++ android/app/src/components/CalendarComponent.tsx
@@ -1,17 +1,17 @@
-// import React from 'react';
-// import { View, Button } from 'react-native';
-// import * as Calendar from 'expo-calendar';
+import React from 'react';
+import { View, Button } from 'react-native';
+import * as Calendar from 'expo-calendar';
 
-// const CalendarComponent = () => {
-//      const checkPermission = async () => {
-//     const { status } = await Calendar.requestCalendarPermissionsAsync();
-//     console.log('Permission:', status);
-//   };
-//   return (
-//     <View style={{ marginTop: 50 }}>
-//       <Button title="Check Calendar Permission" onPress={checkPermission} />
-//     </View>
-//   )
-// }
+const CalendarComponent = () => {
+     const checkPermission = async () => {
+    const { status } = await Calendar.requestCalendarPermissionsAsync();
+    console.log('Permission:', status);
+  };
+  return (
+    <View style={{ marginTop: 50 }}>
+      <Button title="Check Calendar Permission" onPress={checkPermission} />
+    </View>
+  )
+}
 
-// export default CalendarComponent
+export default CalendarComponent
