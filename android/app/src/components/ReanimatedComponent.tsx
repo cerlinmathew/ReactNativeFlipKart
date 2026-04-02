33891cc9261f9a7a3e49ad506d1beb122dd44234
cerlinmathew <cerlinmathew2001@gmail.com>
2026-04-02 18:29:54 +0530

    Revert "fix(animation): separate shared values to control box and image animations independently"
    This reverts commit ba4b2a3892c94131eaffc7c203507015afc36dfb.

--- android/app/src/components/ReanimatedComponent.tsx
+++ android/app/src/components/ReanimatedComponent.tsx
@@ -4,28 +4,15 @@ import { View, Button } from 'react-native';
 import React from 'react';
 
 const ReanimatedComponent = () => {
-  const boxWidth = useSharedValue(100);
-  const imageWidth = useSharedValue(100);
+  const width = useSharedValue(100);
 
   const handlePress = () => {
-    boxWidth.value = withTiming(Math.random() * 100 + 50, { duration: 500 });
+     width.value = withTiming(Math.random() * 100 + 50, { duration: 500 });
   };
 
-  const handlePressimage = () => {
-    imageWidth.value = withTiming(Math.random() * 100 + 50, { duration: 500 });
-  };
-
-
-
-const boxStyle = useAnimatedStyle(() => {
+  const animatedStyle = useAnimatedStyle(() => {
     return {
-      width: boxWidth.value,
-    };
-  });
-
-  const imageStyle = useAnimatedStyle(() => {
-    return {
-      width: imageWidth.value,
+      width: width.value,
     };
   });
 
@@ -37,26 +24,10 @@ const boxStyle = useAnimatedStyle(() => {
             height: 100,
             backgroundColor: 'brown',
           },
-              boxStyle,
+          animatedStyle,
         ]}
       />
       <Button onPress={handlePress} title="Click me" />
-
-      <Animated.Image
-        source={{
-          uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYkNfJXqGRMePB9nR7GLLPoyBL6C-49T8B2w&s', // sample product image
-        }}
-        resizeMode="cover"
-        style={[
-          {
-            height: 150,
-            borderRadius: 12,
-          },
-         imageStyle
-        ]}
-      />
-
-      <Button onPress={handlePressimage} title="Resize Product" />
     </View>
   );
 };
